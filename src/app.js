d054bfc1bf30769f86ab90e381004522016d85b6
rafayel23 <48155011+rafayel23@users.noreply.github.com>
2019-12-20 02:32:03 +0400

    created Signup API with backend validations

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const validator = require('validator');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -22,12 +23,30 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager', {
 });
 
 const User = mongoose.model('User', {
-  name: {
-    type: String
+  firstName: {
+    type: String,
+    required: [true, 'First name is required'],
   },
-  age: {
-    type: Number
+
+  lastName: {
+    type: String,
+    required: [true, 'Last name is required'],
   },
+
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    validate: {
+      validator: value => validator.isEmail(value),
+      message: 'Invalid email format'
+    }
+  },
+
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: [8, 'Password must be at least 8 characters']
+  }
 });
 
 app.get('/users', (_, res) => {
@@ -39,8 +58,11 @@ app.get('/users', (_, res) => {
 app.post('/users', (req, res) => {
   const data = req.body;
   const user = new User(data);
-  user.save().then(_user => {
-    res.send(_user);
+  user.save()
+  .then(result => {
+    res.send(result);
+  }).catch(err => {
+    res.status(400).send(err.errors);
   })
 })
 
